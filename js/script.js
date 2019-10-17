178018e8493fb3e7ad2c34963ee9528781f5ba58
Małgorzata Brożek <gosikbrozek1997@gmail.com>
2019-10-17 13:53:59 +0200

    Change script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ computerMove = 'kamień';
 var playerMove;
 playerMove = 'papier'
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3  + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
