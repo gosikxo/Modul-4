413b33ab4aa6612ca15643bb8003e2553d6744d2
Małgorzata Brożek <gosikbrozek1997@gmail.com>
2019-10-17 16:57:37 +0200

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ if (randomNumber==1) {
 } else {
     printMessage('nożyce');
 }
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+} else {
+    playerMove = 'nieznany ruch';
+}
+
+if(playerMove !== 'nieznany ruch') {
+    printMessage('Twój ruch: ' + playerMove);
+} else {
+    printMessage('Wpisana wartość jest błędna')
+}
